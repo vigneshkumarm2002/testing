8add134632cc5e05e7f9e42938e333a280aaf4bc
vigneshkumarm2002 <mvigneshkumarbvn@gmail.com>
2023-12-11 15:28:34 +0530

    new

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'out', // Change 'out' to your desired output folder
 }
 
 module.exports = nextConfig
