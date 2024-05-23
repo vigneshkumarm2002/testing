7e1b69c5527e98b98d58f483756bc5c6363e1c66
vigneshkumarm2002 <mvigneshkumarbvn@gmail.com>
2024-05-23 13:35:07 +0530

    removed web config

--- src/Environment.js
+++ src/Environment.js
@@ -1,5 +1,4 @@
-export const  Environment = {
-    //apiBaseUrl : 'http://localhost:5063',            //Stagging-Server
-    apiBaseUrl : 'http://111.118.176.28:5064',     //Production-Server
-
+export const Environment = {
+  //apiBaseUrl : 'http://localhost:5063',            //Stagging-Server
+  apiBaseUrl: "http://111.118.176.28:5064", //Production-Server
 };
